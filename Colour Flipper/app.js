aee460829642eb2f2093e57f5ae4920517022784
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2025-02-13 10:47:18 +0000

    Update app.js
    Randomised the background colour generation

--- Colour Flipper/app.js
+++ Colour Flipper/app.js
@@ -6,7 +6,12 @@ const colour = document.querySelector('.colour');
 // Changes the background colour when the button is clicked
 btn.addEventListener('click', function(){
     // TODO: Generate random number between 0 and 3
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colours[randomNumber];
     colour.textContent = colours[randomNumber];
 })
+
+// Generates a random number up to the length of the colours array
+getRandomNumber = () => {
+    return Math.floor(Math.random() * colours.length);
+}
