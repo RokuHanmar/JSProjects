41a76da01a315072fe14912204ddd188f1648719
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2025-02-13 11:03:30 +0000

    Create hex.js
    TODO: add randomness

---
+++ Colour Flipper/hex.js
@@ -0,0 +1,16 @@
+// Define constants and import classes from index.html
+const hex = [0, 1, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+const colour = document.querySelector('.colour');
+
+
+// Changes the background colour when the button is clicked
+btn.addEventListener('click', function() {
+    let hexColour = '#'
+    for (let i = 0; i < 6; i++) {
+        hexColour += hex[0];
+    }
+    colour.textContent = hexColour;
+    document.body.style.backgroundColor = hexColour;
+});
