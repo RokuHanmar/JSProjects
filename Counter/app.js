fdf70b84afb5ab9625e2961e3b8f7df13be46670
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2025-02-13 11:35:04 +0000

    Update app.js
    Changes colour of counter depending on value

--- Counter/app.js
+++ Counter/app.js
@@ -8,6 +8,7 @@ const btns = document.querySelectorAll('.btn');
 btns.forEach(function (btn) {
     btn.addEventListener("click", function (e){
         const styles = e.currentTarget.classList;
+        // changes count on button click
         if (styles.contains('decrease')) {
             count--;
         } else if (styles.contains('increase')) {
@@ -15,6 +16,16 @@ btns.forEach(function (btn) {
         } else if (styles.contains('reset')) {
             count = 0;
         }
+
+        // changes text colour based on value
+        if (count > 0) {
+            value.style.color = 'green';
+        } else if (count < 0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = 'black';
+        }
+        
         value.textContent = count;
     });
 });
